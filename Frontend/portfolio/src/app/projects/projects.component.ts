788a0f9c9afd87e874e0a9e25cdead0857ca665d
Alexander <cleasby.alex@gmail.com>
2018-07-22 11:04:55 -0400

    major changes to stles an added projects

--- Frontend/portfolio/src/app/projects/projects.component.ts
+++ Frontend/portfolio/src/app/projects/projects.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  Projects: {Name:string,Desc:string,Link:string,pic:string}[]
+  constructor() {
+    
+    this.Projects=[
+      {Name:'DCbustracker',Desc:"DC bus tracker is a SPA that uses the user's location to find nearby bus stops and get expected arrival times for buses destined for that stop.  Built on the Ionic framework.  Employs Google Maps api to display stops.  Can be added to a Chrome user's homepage to operate like a native app."
+      ,Link:'https://dcbustracker.com/',pic:'../assets/dcbustracker.com_ScreenGrab.png'},
+      {Name:'Web-Farkle',Desc:"Online multiplayer adaptation of folk dice game, Farkle powered by Node and websockets.",
+    Link:'n/a',pic:'k'}
+    ]
+    
+   }
 
   ngOnInit() {
   }
