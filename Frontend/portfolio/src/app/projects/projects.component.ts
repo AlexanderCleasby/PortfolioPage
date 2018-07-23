c3cecaa57c5cf65b79a2f8a5627cca616474a560
Alexander <cleasby.alex@gmail.com>
2018-07-23 12:49:01 -0400

    added Farkle to project list

--- Frontend/portfolio/src/app/projects/projects.component.ts
+++ Frontend/portfolio/src/app/projects/projects.component.ts
@@ -12,8 +12,12 @@ export class ProjectsComponent implements OnInit {
     this.Projects=[
       {Name:'DCbustracker',Desc:"DC bus tracker is a SPA that uses the user's location to find nearby bus stops and get expected arrival times for buses destined for that stop.  Built on the Ionic framework.  Employs Google Maps api to display stops.  Can be added to a Chrome user's homepage to operate like a native app."
       ,Link:'https://dcbustracker.com/',pic:'../assets/dcbustracker.com_ScreenGrab.png'},
-      {Name:'Web-Farkle',Desc:"Online multiplayer adaptation of folk dice game, Farkle powered by Node and websockets.",
-    Link:'n/a',pic:'k'}
+
+      {Name:'Web-Farkle',
+      Desc:"Online multiplayer adaptation of folk dice game, Farkle powered by Node and websockets."+
+      "  Supports multiple game rooms and players per game.  Game is playable, but as it stands the front end is"+
+      " not so intuitive.  Work in progress.",
+    Link:'https://web-farkle.herokuapp.com/',pic:'../assets/web-farkle.herokuapp.com_ScreenGrab.png'}
     ]
     
    }
