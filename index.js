1d5d994b1c29e13d3c01405db8b44d1301535159
alexandercleasby <cleasby.alex@gmail.com>
2018-12-08 13:33:07 -0500

    overhaul of the file structure to make it less nooby.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname,'/Frontend/portfolio/dist/portfolio')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Frontend/portfolio/dist/portfolio/index.html'));
+    res.sendFile(path.join(__dirname, '/frontend/dist/portfolio/index.html'));
     console.log("New Connection:",req.connection.remoteAddress);
 });
 
