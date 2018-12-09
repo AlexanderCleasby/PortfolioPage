916a4b0daeea378274087d124e913abcd13be8de
alexandercleasby <cleasby.alex@gmail.com>
2018-12-09 14:13:22 -0500

    fixed typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname,'//dist/portfolio')));
+app.use(express.static(path.join(__dirname,'/dist/portfolio')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/portfolio/index.html'));
