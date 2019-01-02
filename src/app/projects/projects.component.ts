3f27e8169e46018a0f15b69f222262af3480027e
alexandercleasby <cleasby.alex@gmail.com>
2019-01-01 23:53:52 -0500

    added your timelog

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,6 +10,9 @@ export class ProjectsComponent implements OnInit {
   constructor() {
     
     this.Projects=[
+      {Name:'Your Timelog',Desc:"React app for tracking how users spend their time using a day planner and charts.  App served from Express, MongoDB used for the database, Google Oauth for authentication."
+      ,Link:'https://your-timelog.herokuapp.com/',pic:'../assets/your-timelog.herokuapp.com_ScreenGrab.png'},
+
       {Name:'DCbustracker',Desc:"DC bus tracker is a SPA that uses the user's location to find nearby bus stops and get expected arrival times for buses destined for that stop.  Built on the Ionic framework.  Employs Google Maps api to display stops.  Can be added to a Chrome user's homepage to operate like a native app."
       ,Link:'https://www.dcbustracker.com/',pic:'../assets/dcbustracker.com_ScreenGrab.png'},
 
